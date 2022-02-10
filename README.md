70e5203b441a569216bd829002bee7cefc884840
James Longmore <1563842+jklong@users.noreply.github.com>
2022-02-10 18:17:32 +1100

    doc: add README.md refactor: update macro lib name and add re-exports feat: add helper declarative macros

---
+++ README.md
@@ -0,0 +1,14 @@
+# applause
+
+Applause is a basic DSL that allows for easy construction of [Clap] `App`s that
+make use of subcommands.
+
+It magicks away the endless `match`es and `enum`s to define subcommands,
+arguments and handlers in favour of a simple dispatch system while passing as
+much as possible back to `clap` via its `derive` API.
+
+## DSL
+
+## Examples
+
+[Clap]: https://github.com/clap-rs/claps
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+// Re-export the main proc macro
+pub use applause_proc_macros::clap_args_with_subcommands;
+
+// Declarative helper macros
+#[macro_export]
+macro_rules! dispatch_args {
+    ($ty:ty => $($args:expr),*) => {
+        <$ty as ::clap::Parser>::parse().cmd.run($($args),*);
+    };
+}
+
+#[macro_export]
+macro_rules! dispatch {
+    ($ty:ty => $($args:expr),*) => {
+        <$ty as ::clap::Parser>::parse().cmd.run($($args),*);
+    };
+    ($ty:ty) => {
+        dispatch_args!($ty => );
+    };
+}
+
+#[macro_export]
+macro_rules! parse_args {
+    ($ty:ty) => {
+        <$ty as ::clap::Parser>::parse()
+    };
 }
